60be03e13dcc7d53d87c8f723a711ea9911e4067
Kirk Clawson <kirk.clawson@varkden.com>
2017-02-04 13:27:18 -0500

    added code for module 1

---
+++ app/events-app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Kirk.Clawson on 2/4/2017.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "events-app",
+    template: "<h2>Hello World</h2>"
+})
+export class EventsAppComponent {
+
+}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Kirk.Clawson on 2/4/2017.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
